8b03dcfe20b0efe8bda1741002c335deb28796f7
Vitaliy <vitaliy.software.developer@gmail.com>
2024-02-12 18:42:06 +0600

    12. Changed EnemySpawner Script - added Enemy Spawning Outside The Screen

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,23 @@ public class EnemySpawner : MonoBehaviour
     public float timeToSpawn;
     private float spawnCounter;
 
+    public Transform minSpawn, maxSpawn;
+
+    private Transform target;
+
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         spawnCounter = timeToSpawn;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            target = player.GetComponent<Transform>();
+        }
     }
 
     // Update is called once per frame
@@ -23,7 +37,45 @@ public class EnemySpawner : MonoBehaviour
         {
             spawnCounter = timeToSpawn;
 
-            Instantiate(enemyToSpawn, transform.position, transform.rotation);
+            Instantiate(enemyToSpawn, SelectSpawnPoint(), transform.rotation);
         }
+
+        transform.position = target.position;
+    }
+
+    public Vector3 SelectSpawnPoint()
+    {
+        Vector3 spawnPoint = Vector3.zero;
+
+        bool spawnVerticalEdge = Random.Range(0f, 1f) > 0.5f;
+
+        if (spawnVerticalEdge)
+        {
+            spawnPoint.y = Random.Range(minSpawn.position.y, maxSpawn.position.y);
+
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                spawnPoint.x = maxSpawn.position.x;
+            }
+            else
+            {
+                spawnPoint.x = minSpawn.position.x;
+            }
+        }
+        else
+        {
+            spawnPoint.x = Random.Range(minSpawn.position.x, maxSpawn.position.x);
+
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                spawnPoint.y = maxSpawn.position.y;
+            }
+            else
+            {
+                spawnPoint.y = minSpawn.position.y;
+            }
+        }
+
+        return spawnPoint;
     }
 }
